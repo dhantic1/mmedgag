5d31583526867235e93f4a7610a95fd87897d711
Mrinmoy Das <mrinmoyfun@gmail.com>
2017-07-26 01:11:08 +0530

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -50,9 +50,7 @@ var app = {
 document.addEventListener('deviceready', function () {
   // Enable to debug issues.
   // window.plugins.OneSignal.setLogLevel({logLevel: 4, visualLevel: 4});
-  if (cordova.platformId == 'android') {
-    StatusBar.backgroundColorByHexString("#333");
-}
+ 
   var notificationOpenedCallback = function(jsonData) {
     alert('notificationOpenedCallback: ' + JSON.stringify(jsonData));
   };
